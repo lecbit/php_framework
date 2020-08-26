c8ffc2e9fc8f0276f0d3dac16062bbaaf97479bd
unknown <artm-marych@rambler.ru>
2020-08-26 17:58:52 +0200

    edit_bd

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -2,10 +2,23 @@
 namespace application\controllers;
 
 use application\core\Controller;
+use application\lib\Db;
 
 class MainController extends Controller{
+    
     public function indexAction(){
-        echo 'Главная страница';
+        $db = new Db;
+
+        $form = '2; DELETE FROM users';
+
+        $params = [
+            'id' => $form,
+        ];
+
+        $data = $db->column('SELECT name FROM users WHERE id = :id', $params);
+        debug($data);
+
+        $this->view->render('Главная страница');
     }
 }
 
